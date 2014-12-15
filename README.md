bd27fd3f54de06eff11312db3189d16061415aa8
andymason <andrew.mason@guardian.co.uk>
2014-12-15 17:57:03 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Interactive stepper
+
+## Installing
+```bash
+> git clone git@github.com:guardian/interactive-stepper.git
+> cd interactive-stepper
+> npm install
+> bower install
+```
+
+## Running
+```bash
+> grunt
+```
+
---
+++ rjsConfig.js
@@ -0,0 +1,27 @@
+require.config({
+        baseUrl: './src/js/app/',
+        inlineText: true,
+        name: '../../../bower_components/almond/almond',
+        preserveLicenseComments: false,
+        include: [
+                'main'
+        ],
+        wrap: {
+                start: 'define([\"require\"],function(require){var req=(function(){',
+                end: 'return require; }()); return req; });'
+        },
+        paths: {
+                almond: '../../../bower_components/almond/almond',
+                backbone: '../../../bower_components/backbone/backbone',
+                'backbone-nested-model': '../../../bower_components/backbone-nested-model/backbone-nested',
+                'backbone.crossdomain': '../../../bower_components/backbone.crossdomain/Backbone.CrossDomain',
+                d3: '../../../bower_components/d3/d3',
+                jquery: '../../../bower_components/jquery/dist/jquery',
+                mustache: '../../../bower_components/mustache/mustache',
+                json: '../../../bower_components/requirejs-json/json',
+                text: '../../../bower_components/requirejs-text/text',
+                underscore: '../../../bower_components/underscore/underscore',
+                iframeMessenger: '../libs/iframeMessenger'
+        }
+});
+
